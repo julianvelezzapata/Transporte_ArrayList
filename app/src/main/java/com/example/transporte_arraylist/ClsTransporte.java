158bc82bf11f9e767bcd8035b3646f520e587fae
julianvelezzapata <71131532+julianvelezzapata@users.noreply.github.com>
2021-10-13 22:53:05 -0500

    metodos

--- app/src/main/java/com/example/transporte_arraylist/ClsTransporte.java
+++ app/src/main/java/com/example/transporte_arraylist/ClsTransporte.java
@@ -3,15 +3,15 @@ package com.example.transporte_arraylist;
 import java.io.Serializable;
 
 public class ClsTransporte implements Serializable {
-    private String transporte;
     private  String codigo;
+    private String transporte;
     private String cantidad;
     private String alimentacion;
     private String total;
 
     public ClsTransporte(String transporte, String codigo, String cantidad, String alimentacion , String total){
-        this.transporte = transporte;
         this.codigo = codigo;
+        this.transporte = transporte;
         this.cantidad = cantidad;
         this.alimentacion = alimentacion;
         this.total = total;
@@ -19,25 +19,34 @@ public class ClsTransporte implements Serializable {
 
     public ClsTransporte(){
     }
+    public String getCodigo(){return codigo;}
+
+    public void setCodigo(String codigo) { this.codigo = codigo;}
+
     public String getTransporte(){return transporte;}
 
     public void setTransporte(String transporte) { this.transporte = transporte;}
-
-    public String getCodigo(){return codigo;}
-
-    public void setCodigo(String transporte) { this.transporte = codigo;}
 
     public String getCantidad(){return cantidad;}
 
-    public void setTransporte(String transporte) { this.transporte = transporte;}
+    public void setCantidad(String cantidad) { this.cantidad = cantidad;}
 
-    public String getTransporte(){return transporte;}
+    public String getAlimentacion(){return alimentacion;}
 
-    public void setTransporte(String transporte) { this.transporte = transporte;}
+    public void setAlimentacion(String alimentacion) { this.alimentacion = alimentacion;}
 
-    public String getTransporte(){return transporte;}
+    public String getTotal(){return total;}
 
-    public void setTransporte(String transporte) { this.transporte = transporte;}
+    public void setTotal(String total) { this.total = total;}
 
-
+    @Override
+    public String toString() {
+        return "Datos transporte{" +
+                "codigo='" + codigo + '\'' +
+                ", transporte='" + transporte + '\'' +
+                ", cantidad='" + cantidad + '\'' +
+                ", alimentacion='" + alimentacion + '\'' +
+                ", total='" + total + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/example/transporte_arraylist/MainActivity.java
+++ app/src/main/java/com/example/transporte_arraylist/MainActivity.java
@@ -11,14 +11,17 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
 
     // creo los objetos que van a almacenar los archivos
     EditText jetcantidad, jetcodigo;
     CheckBox  jcbalimentacion;
     RadioButton jrbavion, jrbmoto, jrbbus;
-    RadioGroup jrgtransporte;
     TextView jtvtotal;
+    ClsTransporte objtransporte = new ClsTransporte();
+    ArrayList<ClsTransporte> altransporte;
     int pos, sw=0;
 
 
@@ -37,14 +40,13 @@ public class MainActivity extends AppCompatActivity {
         jrbmoto=findViewById(R.id.rbmoto);
         jrbbus=findViewById(R.id.rbbus);
         jtvtotal=findViewById(R.id.tvtotal);
-        jrgtransporte = findViewById(R.id.rgtransporte);
+
+
         jetcodigo = findViewById(R.id.etcodigo);
+        altransporte = new ArrayList<ClsTransporte>();
     }
 
-    public void Calcular(View view
-
-
-    ){
+    public void Calcular(View view){
         String cantidad;
         cantidad = jetcantidad.getText().toString();
 
@@ -80,4 +82,102 @@ public class MainActivity extends AppCompatActivity {
         jetcantidad.requestFocus();
         sw=0;
     }
+
+    public void  Guardar(View view) {
+        String codigo, transporte = null, cantidad , alimentacion, total, bus, avion, moto;
+        codigo = jetcodigo.getText().toString();
+        cantidad = jetcantidad.getText().toString();
+        alimentacion = jcbalimentacion.getText().toString();
+        total = jtvtotal.getText().toString();
+        bus = jrbbus.getText().toString();
+        avion = jrbavion.getText().toString();
+        moto = jrbmoto.getText().toString();
+
+
+        if (cantidad.isEmpty() || codigo.isEmpty()) {
+            Toast.makeText(this, "llenar los campos", Toast.LENGTH_LONG).show();
+        } else {
+                    if(sw==1){
+                                if (jrbavion.isChecked())
+                                        transporte = avion;
+                                        altransporte.get(pos).setTransporte(transporte);
+                                        if (jrbmoto.isChecked())
+                                        transporte = moto;
+                                        altransporte.get(pos).setTransporte(transporte);
+                                        if  (jrbbus.isChecked()) {
+                                            transporte = bus;
+                                            altransporte.get(pos).setTransporte(transporte);
+                                        }
+
+                                altransporte.get(pos).setCodigo(codigo);
+                                altransporte.get(pos).setCantidad(cantidad);
+                                altransporte.get(pos).setAlimentacion(alimentacion);
+                                altransporte.get(pos).setTotal(total);
+                                Toast.makeText(this, "Registro modificado", Toast.LENGTH_SHORT).show();
+                                limpiar_campos();
+                                sw = 0;
+                               }
+                                else{
+                                    pos=0;
+                                    sw=0;
+                                    while( pos < altransporte.size() && sw ==0){
+                                        objtransporte = altransporte.get(pos);
+                                        if (objtransporte.getCodigo().equals(codigo))
+                                            sw=1;
+                                        else
+                                            pos++;
+                                    }
+                                    if(sw == 0){
+                                        objtransporte = new ClsTransporte(codigo, transporte, cantidad, alimentacion,total);
+                                        altransporte.add(objtransporte);
+                                        Toast.makeText(this, "Registro adicionado", Toast.LENGTH_SHORT).show();
+                                        limpiar_campos();
+                                    }
+                                    else {
+                                        limpiar_campos();
+                                        Toast.makeText(this, "Estudiante ya registrado", Toast.LENGTH_SHORT).show();
+                                        sw=0;
+                                    }
+                                }
+
+
+                 }
+
+
+
+    }
+
+    public void consultar (View view){Consultar_Transporte();}
+
+    private void Consultar_Transporte(){
+        String carnet;
+        pos=0;
+        sw=0;
+        carnet=jetcodigo.getText().toString();
+        if (carnet.isEmpty()){
+            Toast.makeText(this, "numero de codigo es requerido", Toast.LENGTH_LONG).show();
+            jetcodigo.requestFocus();
+        }
+        else{
+            while (pos < altransporte.size() && sw==0){
+                objtransporte=altransporte.get(pos);
+                if (objtransporte.getCodigo().equals(carnet))
+                    sw=1;
+                else
+                    pos++;
+            }
+            if (sw == 0)
+                Toast.makeText(this, "codigo no registrado", Toast.LENGTH_SHORT).show();
+            else{
+                jetcantidad.setText(objtransporte.getCantidad());
+                jtvtotal.setText(objtransporte.getCantidad());
+
+            }
+            jetcodigo.requestFocus();
+        }
+    }
+
+
+
+
 }
